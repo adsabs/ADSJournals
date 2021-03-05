b2a047e60a32c6484927fdb99cb0a3e694800e7b
Matthew Templeton <mtempleton@kinglet.local>
2021-03-05 08:39:04 -0500

    modified:   config.py

--- config.py
+++ config.py
@@ -8,18 +8,18 @@ COLLECTIONS = ['ast', 'phy', 'gen']
 JDB_DATA_DIR = '/proj/ads/abstracts/config/journalsdb/PIPELINE/data/'
 
 # BIBSTEMS has bibstem, R/J/C/etc, and canonical name
-BIBSTEMS_FILE = 'bibstems.dat'
+BIBSTEMS_FILE = JDB_DATA_DIR + 'bibstems.dat'
 
 # JOURNAL_ABBREV has bibstem and multiple title abbreviations (e.g.
 # A&A, AA, Astron. & Astrophys.)
-JOURNAL_ABBREV_FILE = 'journals_abbrev.dat'
+JOURNAL_ABBREV_FILE = JDB_DATA_DIR + 'journals_abbrev.dat'
 
-JOURNAL_ISSN_FILE = 'journal_issn'
-ISSN_JOURNAL_FILE = 'issn2journal'
-CANONICAL_BIB_FILE = 'bib2accno.dat'
+JOURNAL_ISSN_FILE = JDB_DATA_DIR + 'journal_issn'
+ISSN_JOURNAL_FILE = JDB_DATA_DIR + 'issn2journal'
+CANONICAL_BIB_FILE = JDB_DATA_DIR + 'bib2accno.dat'
 
 # RASTERIZING.xml directory
-RASTER_CONFIG_DIR = 'raster_config/'
+RASTER_CONFIG_DIR = JDB_DATA_DIR + 'raster_config/'
 
 # ESOURCES: for holdings table
 # Any new esources must be **prepended** to this list
